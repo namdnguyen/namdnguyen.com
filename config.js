a38804dd564134bcc698afa338580c58c0c41d09
Nam D. Nguyen <nnguyen83@gmail.com>
2019-07-20 15:27:46 -0500

    Change site subtitle

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Nam D. Nguyen',
+  subtitle: 'Contemplating the world with data and reason.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
